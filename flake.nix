593928a66b4e8f9ded613a58c52dd63a2c60f555
LaiMicK <hyprland@vk.com>
2024-12-05 10:48:20 +0500

    add start configurations

---
+++ flake.nix
@@ -0,0 +1,38 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    home-manager = {
+      url = "github:nix-community/home-manager/release-24.11";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = inputs@{ nixpkgs, home-manager, ... }: let
+    system = "x86_64-linux"; # Определите систему
+    pkgs = import nixpkgs { inherit system; }; # Импортируйте pkgs
+  in {
+    nixosConfigurations = {
+      nixos = nixpkgs.lib.nixosSystem {
+        inherit system;
+        modules = [
+          ./configuration.nix
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.laimick = import ./home.nix;
+          }
+        ];
+      };
+    };
+
+    # Добавьте эту секцию для home-manager
+    homeConfigurations = {
+      laimick = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+        modules = [ ./home.nix ];
+      };
+    };
+  };
+}
+
