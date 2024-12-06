d9c46a431ba36c75e98b264256cfacd701e18b1b
LaiMicK <hyprland@vk.com>
2024-12-06 11:08:31 +0500

    add PipeWire

--- configuration.nix
+++ configuration.nix
@@ -66,6 +66,16 @@
   
   services.xserver.videoDrivers = ["nvidia"];
   
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+  };
+  
   hardware.nvidia = {
     modesetting.enable = true;
     powerManagement.enable = false;
