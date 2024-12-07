42814962e1b0c8c8b24c998f4030dcf9499dcaf8
LaiMicK <hyprland@vk.com>
2024-12-07 20:15:02 +0500

    settings hyprland

--- home/programs/hyprland/hypr.nix
+++ home/programs/hyprland/hypr.nix
@@ -50,9 +50,9 @@
           enabled = true;
           color = "rgba(237, 133, 232, 0.1)";
           color_inactive = "rgba(163, 88, 232, 0.4)";
-          range = 15;
-          render_power = 1;
-          scale = 0.5;
+          range = 50;
+          render_power = 2;
+          scale = 0.75;
         };
       };
 
