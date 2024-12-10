847d9dec56849c4b1c6d248e7bef7a0df1ccd881
LaiMicK <hyprland@vk.com>
2024-12-10 14:25:28 +0500

    fix hyprland config

--- home/programs/wayland/hyprland/default.nix
+++ home/programs/wayland/hyprland/default.nix
@@ -11,6 +11,5 @@
     enable = true;
 
     package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
-    portalPackage = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
   };
 }
